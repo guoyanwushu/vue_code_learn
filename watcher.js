acb505e0cca75971623b68170a480240ba067430
yingang <495197461@qq.com>
2018-04-28 17:55:49 +0800

    新增了构造函数 添加监视器函数

--- watcher.js
+++ watcher.js
@@ -1,15 +1,41 @@
 /**
  *
  */
+// 伪构造函数
+function Vue(options) {
+    this.options = options;
+    addListener(this,options.data);
+}
+// 添加监视器
+function addListener(vm,options) {
+    var data = options.data;
+    for (var key in data) {
+        Object.defineProperty(data,'key',{
+            configurable:false,
+            enumerable:true,
+            get: function() {
+                return vm[key];
+            },
+            set: function(newval) {
+                // 通知监视器
+                watcher(key,newval);
+                wm[key] = newval;
+            }
+        })
+    } 
+}
+// 监视器
 function watcher(key,val) {
     // 自身维护一个对应的变量数组
     var latestSet = [];
     if (latestSet[key]!=val) {
         latestSet[key] = val;
-        // 检测到数据变化,需要更新视图
+        // 检测到数据变化,需要更新视图  也不能立即就调用视图刷新 ，如果一定定时器1s内改了10000次，结果又变成最初始的值,那不是要去渲染一万次，还没渲染完数据又变了。所以应该加一个限制，把数据推到一个队列里，等
+        // 队列里的数据到了一定规模后整体刷新。
         updateView(latestSet);
     }
 }
+// 视图更新器
 function  updateView(dataset) {
     // 根据最新的变量值,生成对应的新的文档片段
 }
@@ -38,10 +64,6 @@ Vue.prototype  = {
             }
             else if(dom.nodeType==3){
                 // 编译文本节点
-                var reg = /\{\{\w*\}\}/;
-                if(resg)
-
-
             }
         }
     },
@@ -56,7 +78,7 @@ Vue.prototype  = {
             }
             if (attr.name.test(eventReg)) {
                 node.addEventListener(RegExp.$1,function () {
-                    vm[attr.name];
+                    vm[attr.name]();
                 },false)
             }
             if (node.childNodes&&node.childNodes.length!=0) {
@@ -68,12 +90,12 @@ Vue.prototype  = {
         })
 
     },
+    // 编译文本节点
     compileTextNode: function (dom,vm) {
         var nodeValue = dom.textContent,
             reg = /\{\{\w*\}\}/;
         if (reg.test(nodeValue)) {
             dom.textContent = nodeValue.replace(reg,vm[RegExp.$1]);
         }
-
     }
 }
